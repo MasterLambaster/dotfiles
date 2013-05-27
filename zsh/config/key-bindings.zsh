3967c83e2e93595d59d9277b2c876796923b876c
MasterLambaster <masterlambaster@gmail.com>
2013-05-27 22:58:09 +0300

    Add proper movement key bindings

---
+++ zsh/config/key-bindings.zsh
@@ -0,0 +1,9 @@
+bindkey -v                                          # Use vi key bindings
+
+bindkey -s '\el' 'ls\n'                             # [Esc-l] - run command: ls
+bindkey -s '\e.' '..\n'                             # [Esc-.] - run command: .. (up directory)
+bindkey '^r' history-incremental-search-backward    # [Ctrl-r] - Search backward incrementally for a specified string. The string may begin with ^ to anchor the search to the beginning of the line.
+
+# emacs style
+bindkey '^a' beginning-of-line
+bindkey '^e' end-of-line
