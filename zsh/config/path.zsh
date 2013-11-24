4e9492d7106940a2c33a49d93ae885e2ab01768b
MasterLambaster <masterlambaster@gmail.com>
2013-11-24 16:41:06 +0200

    Fix homeberw path

--- zsh/config/path.zsh
+++ zsh/config/path.zsh
@@ -1,4 +1,7 @@
 export GOPATH=/usr/local/var/go
 
+# To make homebrew work properly
+PATH="/usr/local/bin:$PATH"
+
 PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
 PATH=$PATH:/usr/local/var/go/bin # Go Path
