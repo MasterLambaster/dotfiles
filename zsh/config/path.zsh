3c013f9d212dcb08e6eb78476b19532255a4ebc1
MasterLambaster <masterlambaster@gmail.com>
2014-04-15 10:42:40 +0300

    Configure nodejs paths

--- zsh/config/path.zsh
+++ zsh/config/path.zsh
@@ -1,7 +1,11 @@
 export GOPATH=/usr/local/var/go
 
+# NodeJs
+export NODE_PATH="/usr/local/lib/node"
+
 # To make homebrew work properly
 PATH="/usr/local/bin:$PATH"
 
 PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
 PATH=$PATH:/usr/local/var/go/bin # Go Path
+PATH=$PATH:/usr/local/share/npm/bin
